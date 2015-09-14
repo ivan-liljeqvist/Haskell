a87b087253ed128e63e76163517d70163b752cd5
ivan-liljeqvist <ivanli@kth.se>
2015-09-14 15:16:27 +0200

    F2

--- F2/F2.hs
+++ F2/F2.hs
@@ -213,6 +213,11 @@ module F2 where
     -}
 
     makeRelativeMatrix :: [[(Char, Int)]] -> Int -> [[(Char, Double)]]
+    --1) We need to go through each tuple and divide the Double value by the number of sequences
+    --2) Go through each row in the matrix.
+    --3) Go through each tuple in that row.
+    --4) For each tuple divide the second value (y) by the number of sequences.
+    --5) Now we have a relative matrix.
     makeRelativeMatrix absMatrix numberOfSeqs = map (map (\(x, y) -> (x , fromIntegral y / fromIntegral (numberOfSeqs)))) absMatrix
 
 
